4a32a20f25d0eb537a690ab11d5b8e53ee545d33
NamrataCE <NamrataCE@192.168.0.107>
2020-12-30 13:28:12 +0900

    Spring boot file upload

---
+++ SpringUpload/src/main/java/com/select/model/Form.java
@@ -0,0 +1,46 @@
+package com.select.model;
+
+public class Form {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String file;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	@Override
+	public String toString() {
+		return "Form [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password=" + password
+				+ ", file=" + file + "]";
+	}
+	
+}
